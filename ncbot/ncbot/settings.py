4f8d1f864a8f7f13a31a9473cbe8721a77559a64
shaggy-axel <kornerus@fjfi.cvut.cz>
2021-04-07 18:05:34 +0300

    modified

--- ncbot/ncbot/settings.py
+++ ncbot/ncbot/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-from db_env import DBNAME, DBUSER, DBPASS, DBHOST, DBPORT
+from db_env import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
